7808ef6e3a05973d1959109f77892f9f3da125d6
Sod4H <manuelpazlorenzo@gmail.com>
2024-03-08 14:44:11 -0300

    primera-Preentrega

---
+++ routes/cart.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+
+
+router.post('/', (req, res) => {
+
+});
+
+
+router.get('/:cid', (req, res) => {
+
+});
+
+
+router.post('/:cid/product/:pid', (req, res) => {
+
+});
+
+module.exports = router;
---
+++ routes/products.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+
+
+router.get('/', (req, res) => {
+
+});
+
+
+router.get('/:pid', (req, res) => {
+
+});
+
+
+router.post('/', (req, res) => {
+
+});
+
+
+router.put('/:pid', (req, res) => {
+
+});
+
+
+router.delete('/:pid', (req, res) => {
+
+});
+
+module.exports = router;
